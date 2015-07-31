197ee6d27ac9f56f14a25c40db7623ad478ec168
suportemarco <suporte_marco@hotmail.com>
2015-07-31 11:44:51 -0300

    Added ISO strings

---
+++ app/src/main/java/com/bematech/material/models/GenericModel.java
@@ -0,0 +1,12 @@
+package com.bematech.material.models;
+
+/**
+ * Created by c1401895 on 30/07/2015.
+ */
+public class GenericModel {
+
+    private int id;
+    private String name;
+    private String imageUrl;
+
+}
